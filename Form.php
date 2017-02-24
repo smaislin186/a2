4b2cabe55843f464777c8ffc0bffadd111b1fdba
smaislin186 <samantha.maislin@gmail.com>
2017-02-23 22:30:09 -0500

    bug fix in Form.php

--- Form.php
+++ Form.php
@@ -151,8 +151,8 @@ class Form {
         }
 
         # Set public property hasErrors as Boolean
-        $this->hasErrors = empty($errors);
-
+        $this->hasErrors = !empty($errors);
+        
         return $errors;
 
     }
